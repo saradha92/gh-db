93d2140e21ab60b90aafa37a53fa0899187799f6
Saradha Boothalingham <saradha.b@ebmpapst.local>
2022-12-13 13:02:39 +0530

    updated env secrets

--- data/database.js
+++ data/database.js
@@ -1,18 +1,18 @@
 import { MongoClient } from 'mongodb';
 
-const clusterAddress = process.env.MONGODB_URL;
-const dbUser = process.env.MONGODB_USERNAME;
-const dbPassword = process.env.MONGODB_PASSWD;
-const dbName = process.env.MONGODB_DB_NAME;
+const MONGODB_URL = process.env.MONGODB_URL;
+const MONGODB_USERNAME = process.env.MONGODB_USERNAME;
+const MONGODB_PASSWD = process.env.MONGODB_PASSWD;
+const MONGODB_NAME = process.env.MONGODB_DB_NAME;
 
-const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWD}@${MONGODB_URL}/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 
 console.log('Trying to connect to db');
 
 try {
   await client.connect();
-  await client.db(dbName).command({ ping: 1 });
+  await client.db(MONGODB_NAME).command({ ping: 1 });
   console.log('Connected successfully to server');
 } catch (error) {
   console.log('Connection failed.');
