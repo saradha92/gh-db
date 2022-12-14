00ea9e5c0540b813b7a74c014103e0ec754f8e9e
Saradha Boothalingham <saradha.b@ebmpapst.local>
2022-12-14 17:54:54 +0530

    updated db

--- data/database.js
+++ data/database.js
@@ -1,19 +1,19 @@
 import { MongoClient } from 'mongodb';
 
-const MONGODB_URL = process.env.MONGODB_URL;
-const MONGODB_USERNAME = process.env.MONGODB_USERNAME;
-const MONGODB_PASSWD = process.env.MONGODB_PASSWD;
-const MONGODB_NAME = process.env.MONGODB_NAME;
-const MONGODB_PORT = process.env.MONGODB_PORT
+const clusterAddress = process.env.MONGODB_URL;
+const dbuser = process.env.MONGODB_USERNAME;
+const dbpassword = process.env.MONGODB_PASSWD;
+const dbname = process.env.MONGODB_NAME;
+const MONGODB_PORT = process.env.MONGODB_PORT;
 
-const uri = `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWD}@${MONGODB_URL}/{MONGODB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${dbuser}:${dbpassword}@${clusterAddress}/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 
 console.log('Trying to connect to db');
 
 try {
   await client.connect();
-  await client.db(MONGODB_NAME).command({ ping: 1 });
+  await client.db(dbname).command({ ping: 1 });
   console.log('Connected successfully to server');
 } catch (error) {
   console.log('Connection failed.');
@@ -21,6 +21,6 @@ try {
   console.log('Connection closed.');
 }
 
-const database = client.db(MONGODB_NAME);
+const database = client.db(dbname);
 
 export default database;
--- playwright.config.js
+++ playwright.config.js
@@ -5,13 +5,13 @@
 const config = {
   testDir: './tests',
   /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
+  timeout: 15 * 1,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 5000,
+    timeout: 15,
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
